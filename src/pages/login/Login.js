2b6176d8a35cfaabba802367fb44aa05cc10fe2b
Karlo11111 <97040384+Karlo11111@users.noreply.github.com>
2024-03-15 19:41:41 +0100

    login working successfully

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -8,29 +8,30 @@ export default function Login() {
   const navigate = useNavigate();
   const signIn = (e) => {
     e.preventDefault();
-    
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
-    signInWithEmailAndPassword(auth, email, password).then((userCredential) => {
-      //success full sign in
-      const user = userCredential.user;
-      console.log(user);
-      navigate('/home');
-    }).catch((error) => { 
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        //success full sign in
+        const user = userCredential.user;
+        console.log(user);
+        navigate("/home");
+      })
+      .catch((error) => {
         const errorCode = error.code;
         const errorMessage = error.message;
         console.error("Error code:", errorCode, "Error Message:", errorMessage);
-    });
-  }
+      });
+  };
 
   return (
     <>
-      <form>
+      <form onSubmit={signIn}>
         <h3>Sign in</h3>
-        <input type="text" placeholder="Email" />
-        <input type="password" placeholder="Password" />
-        <button type="submit" onClick={signIn()} >Sign in</button>
+        <input id="email" type="text" placeholder="Email" />
+        <input id="password" type="password" placeholder="Password" />
+        <button type="submit">Sign in</button>
         <div className="register-btn-div">
           <p>Don't have an account?</p>
           <Link to="/register">
